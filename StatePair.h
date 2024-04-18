d38170cea44f1aa7d6417e9a12b9a510be90567c
Michael Llanes <167344314+MALLANES@users.noreply.github.com>
2024-04-17 23:40:50 -0500

    Add initial files. Broken and need fixing.

---
+++ StatePair.h
@@ -0,0 +1,36 @@
+#ifndef STATEPAIR
+#define STATEPAIR
+
+template <typename T1, typename T2>
+class StatePair
+{
+    private: 
+    T1 StateKey;
+    T2 StateValue;
+
+    // TODO: Define constructors
+    public: 
+    StatePair();
+    StatePair(T1 userKey, T2 userValue);
+
+    // TODO: Define mutators, and accessors for StatePair
+    T1 GetKey() {
+        return StateKey;
+    }
+    T2 GetValue() {
+        return StateValue;
+    }
+
+    void SetKey(T1 key) {
+        StateKey = key;
+    }
+    void SetValue(T2 value) {
+        StateValue = value;
+    }
+    // TODO: Define PrintInfo() method
+    void PrintInfo() {
+
+    }
+};
+
+#endif
