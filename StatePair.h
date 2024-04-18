f6f1a7344d6e616d407cd624edea73e54bd519c0
Michael Llanes <167344314+MALLANES@users.noreply.github.com>
2024-04-18 18:32:48 -0500

    Update StatePair.h. StatePair is complete now.

--- StatePair.h
+++ StatePair.h
@@ -4,32 +4,43 @@
 template <typename T1, typename T2>
 class StatePair
 {
-    private: 
+private:
     T1 StateKey;
     T2 StateValue;
 
     // TODO: Define constructors
-    public: 
-    StatePair();
-    StatePair(T1 userKey, T2 userValue);
+public:
+    StatePair()
+    {
+    }
+    StatePair(T1 userKey, T2 userValue)
+    {
+        StateKey = userKey;
+        StateValue = userValue;
+    }
 
     // TODO: Define mutators, and accessors for StatePair
-    T1 GetKey() {
+    T1 GetKey()
+    {
         return StateKey;
     }
-    T2 GetValue() {
+    T2 GetValue()
+    {
         return StateValue;
     }
 
-    void SetKey(T1 key) {
+    void SetKey(T1 key)
+    {
         StateKey = key;
     }
-    void SetValue(T2 value) {
+    void SetValue(T2 value)
+    {
         StateValue = value;
     }
     // TODO: Define PrintInfo() method
-    void PrintInfo() {
-
+    void PrintInfo()
+    {
+        cout << StateKey << ": " << StateValue << endl;
     }
 };
 
